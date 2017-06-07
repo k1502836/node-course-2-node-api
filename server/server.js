6eec695ff2eae7d65d8ebb2942c95af06f27bbb8
k1502836 <k1502836@kcl.ac.uk>
2017-06-07 11:16:24 +0100

    update todos route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -61,23 +62,36 @@ app.get('/todos/:id', (req, res) => {
 });
 
 // ##############################################
-// UPDATE Todo
-// app.get('/todos/:id', (req, res) => {
-// 	var id = req.params.id;
+// UPDATE Todos
+app.patch('/todos/:id', (req, res) =>{
+	var id = req.params.id;
 
-// 	if(!ObjectID.isValid(id)){
-// 		return res.status(404).send();
-// 	}
+	var body = _.pick(req.body, ['text', 'completed']);
 
-// 	Todo.findById(id).then((todo) =>{
-// 		if(!todo) {
-// 			return res.status(404).send();
-// 		}
-// 		res.send({todo});
-// 	}).catch((e) => {
-// 		res.status(400).send();
-// 	});
-// });
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	if(_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	}else{
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {
+		$set:body
+	},{
+		new:true
+	}).then((todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
 
 // ##############################################
 // DELETE Todo
